9550356cffb27084f1007cff3f5cc85d60e0d735
ptsand <ptrsand@gmail.com>
2022-09-24 15:05:36 +0200

    move nodemon to devDependencies, prepare for heruku deployment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/clock.html"));
 });
 
-const port = process.env.NETLIFY ? 80 : 8080; // use port 80 on netlify
+const port = process.env.PORT || 8080
 app.listen(port, (error) => {
     console.log("Server is running on port", port);
 });
